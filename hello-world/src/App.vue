09345b37b6a7f04d34f5463e3d76449caa4549f4
maxlivit <maxim.livit22@sitechhs.com>
2021-10-27 11:46:55 -0400

    ummmmmm

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/Dresser">Dresser</router-link>
-    </div>
-    <router-view/>
+    <Dresser />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Dresser from "./components/Dresser.vue"
+export default {
+  name: 'App',
+  components: {
+    Dresser
   }
 }
+</script>
+
+
+<style lang="scss">
+
 </style>
---
+++ hello-world/src/components/Dresser.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="main">
+      <header class="header"></header>
+         <h1 class="h1">Build Your Fit</h1>
+      <img class = "logo" src = "../assets/livitlogo.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Dresser',
+  data() {
+      return {
+
+      }
+  }, 
+  methods: {
+
+  },
+  computed: {
+
+  }
+}
+
+</script>
+
+<style scoped>
+.logo {
+    width: 20rem;
+    margin-top: -5rem;
+}
+
+.h1 {
+    text-align: center;
+    font-family: ;
+}
+.header {
+
+}
+
+</style>
