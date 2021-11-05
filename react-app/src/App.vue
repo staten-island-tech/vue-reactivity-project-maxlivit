2adc72fda455f0b978e016e5f559a606282541c0
maxlivit <maxim.livit22@sitechhs.com>
2021-11-05 12:27:17 -0400

    oh em gee

---
+++ react-app/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="nav-bar">
+       <img class = "logo" alt="Vue logo" src="./assets/livitlogo.png">
+       <div class="nav-buttons">
+         <button>hats</button>
+         <button>shirts</button>
+         <button>hoodies</button>
+         <button>pants</button>
+         <button>shorts</button>
+         <button>shoes</button>
+         <button>socks</button>
+         </div>
+
+      </div> 
+   
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+.logo {
+  width: 11rem;
+  height: 8rem;
+}
+
+.nav-bar {
+  background-color: lightblue;
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  justify-content: space-between;
+}
+
+
+</style>
