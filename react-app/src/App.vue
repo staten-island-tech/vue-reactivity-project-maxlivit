be2ee5203c9aa6a294bb6cab4d9504ff3ee4b854
maxlivit <maxim.livit22@sitechhs.com>
2021-11-10 12:26:39 -0500

    divs

--- react-app/src/App.vue
+++ react-app/src/App.vue
@@ -13,21 +13,17 @@
          </div>
 
       </div> 
-   
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div class="selecting">
 
+     </div>
+  <div class="add"> 
+    </div> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
 </script>
 
 <style lang="scss">
--- react-app/src/components/HelloWorld.vue
+++ react-app/src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: '',
   props: {
     msg: String
   }
