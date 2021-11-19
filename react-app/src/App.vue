aae36874009fce63d3bad7b276a56f1357f6b8e0
maxlivit <maxim.livit22@sitechhs.com>
2021-11-19 12:28:56 -0500

    lol

--- react-app/src/App.vue
+++ react-app/src/App.vue
@@ -3,15 +3,19 @@
     <div class="nav-bar">
        <img class = "logo" alt="Vue logo" src="./assets/livitlogo.png">
        <div class="nav-buttons">
-         <button @click="poop(item)" v-for="item in clothing" :key="item">{{item}}</button>
+         <button @click="chosen(item)" v-for="item in clothingNames" :key="item">{{item}}</button>
          </div>
 
       </div> 
-   <div class="show-img" v-for="item in clothing" :key="item" v-if="item=currentItem">
-     
+   <div class="show-img" v-for="item in clothing" :key="item">
+     <h1>{{item.category}}</h1>
+     <img :src="item.url">
      </div>
   <div class="add"> 
     </div> 
+  <div class="cart">
+    <p>cart({{cart}})</p>
+    </div>
   </div>
 </template>
 
@@ -20,7 +24,7 @@
     data() {
       return {
         currentItem: "",
-        clothing: [
+        clothingNames: [
           "hats", 
           "shirts", 
           "hoodies", 
@@ -29,52 +33,52 @@
           "socks", 
           "shoes",
         ],
-        hats: [
+        clothing: [
           {
             name: "Yellow Hat",
+            category: "hat",
             url: require("@/assets/yellowhat.png")
           },
-        ], 
-        shirts: [
+         { name:"Blue Hat",
+          category: "hat",
+          url: require("@/assets/bluehat.png")
+         },
           {
             name: "Yellow Shirt", 
+            category: "shirts",
             url: require("@/assets/yellowshirt.png")
-          }
-        ],
-        hoodies: [
+          },
           {
-            name: "Yellow Shirt", 
+            name: "Yellow Hoodie", 
+            category: "hoodies",
             url: require("@/assets/yellowhoodie.png")
-          }
-        ],
-        pants: [
+          },
           {
-            name: "Yellow Shirt", 
+            name: "Yellow Pants", 
+            category: "pants",
             url: require("@/assets/yellowpants.png")
-          }
-        ],
-        shorts: [
+          },
           {
-            name: "Yellow Shirt", 
+            name: "Yellow Shorts", 
+            category: "shorts",
             url: require("@/assets/yellowshorts.png")
-          }
-        ],
-        socks: [
+          },
           {
-            name: "Yellow Shirt", 
+            name: "Yellow Socks", 
+            category: "socks",
             url: require("@/assets/yellowsocks.png")
-          }
-        ],
-        shoes: [
+          },
           {
-            name: "Yellow Shirt", 
+            name: "Yellow Shoes", 
+            category: "shoes",
             url: require("@/assets/yellowshoes.png")
-          }
+          },
         ],
+        cart: 0
       }
     }, 
     methods: {
-      poop(item) {
+      chosen(item) {
         this.currentItem = item
       }
       },
