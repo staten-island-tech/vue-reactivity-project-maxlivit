633ab2dee80dcbac573bba3b65c4b38448b21e6e
maxlivit <maxim.livit22@sitechhs.com>
2021-11-24 11:44:07 -0500

    update

--- react-app/src/App.vue
+++ react-app/src/App.vue
@@ -3,57 +3,99 @@
     <div class="nav-bar">
        <img class = "logo" alt="Vue logo" src="./assets/livitlogo.png">
        <div class="nav-buttons">
-         <button @click="showHats = true, 
+         <button @click="
+          showHats = true, 
           showShirts = false,
-          showHoodies = false">Hats</button>
-         <button @click="showHats = true">Shirts</button>
-         <button @click="showHats = true">Hoodies</button>
-         <button @click="showHats = true">Pants</button>
-         <button @click="showHats = true">Shorts</button>
-         <button @click="showHats = true">Socks</button>
-         <button @click="showHats = true">Shoes</button>
+          showHoodies = false, 
+          showPants = false,
+          showShorts = false,
+          showSocks = false, 
+          showShoes = false"
+          >Hats</button>
+         <button @click="showHats = false, 
+          showShirts = true,
+          showHoodies = false, 
+          showPants = false,
+          showShorts = false,
+          showSocks = false, 
+          showShoes = false">Shirts</button>
+         <button @click="showHats = false, 
+          showShirts = false,
+          showHoodies = true, 
+          showPants = false,
+          showShorts = false,
+          showSocks = false, 
+          showShoes = false">Hoodies</button>
+         <button @click="showHats = false, 
+          showShirts = false,
+          showHoodies = false, 
+          showPants = true,
+          showShorts = false,
+          showSocks = false, 
+          showShoes = false">Pants</button>
+         <button @click="showHats = false, 
+          showShirts = false,
+          showHoodies = false, 
+          showPants = false,
+          showShorts = true,
+          showSocks = false, 
+          showShoes = false">Shorts</button>
+         <button @click="showHats = false, 
+          showShirts = false,
+          showHoodies = false, 
+          showPants = false,
+          showShorts = false,
+          showSocks = true, 
+          showShoes = false">Socks</button>
+         <button @click="showHats = false, 
+          showShirts = false,
+          showHoodies = false, 
+          showPants = false,
+          showShorts = false,
+          showSocks = false, 
+          showShoes = true">Shoes</button>
          </div>
 
       </div> 
       <section class='hat-section' v-if="showHats"> 
         <div class="show-hats" v-for="hat in hats" :key="hat" >
-     <h1>{{hat.category}}</h1>
+     <h1>{{hat.name}}</h1>
      <img :src="hat.url">
      </div>
         </section> 
    <section class='shirt-section' v-if="showShirts"> 
      <div class="show-shirts" v-for="shirt in shirts" :key="shirt">
-     <h1>{{shirt.category}}</h1>
+     <h1>{{shirt.name}}</h1>
      <img :src="shirt.url">
      </div>
       </section>
       <section class='hoodie-section' v-if="showHoodies"> 
      <div class="show-hoodies" v-for="hoodie in hoodies" :key="hoodie">
-     <h1>{{hoodie.category}}</h1>
+     <h1>{{hoodie.name}}</h1>
      <img :src="hoodie.url">
      </div>
      </section>
      <section class='pants-section' v-if="showPants"> 
      <div class="show-pants" v-for="pant in pants" :key="pant">
-     <h1>{{pant.category}}</h1>
+     <h1>{{pant.name}}</h1>
      <img :src="pant.url">
      </div>
      </section>
      <section class='shorts-section' v-if="showShorts"> 
      <div class="show-shorts" v-for="short in shorts" :key="short">
-     <h1>{{short.category}}</h1>
+     <h1>{{short.name}}</h1>
      <img :src="short.url">
      </div>
      </section>
      <section class='sock-section' v-if="showSocks"> 
      <div class="show-socks" v-for="sock in socks" :key="sock">
-     <h1>{{sock.category}}</h1>
+     <h1>{{sock.name}}</h1>
      <img :src="sock.url">
      </div>
      </section>
      <section class='shoe-section' v-if="showShoes"> 
      <div class="show-shoes" v-for="shoe in shoes" :key="shoe">
-     <h1>{{shoe.category}}</h1>
+     <h1>{{shoe.name}}</h1>
      <img :src="shoe.url">
      </div>
      </section>
@@ -62,6 +104,7 @@
   <div class="cart">
     <p>cart({{cart}})</p>
     </div>
+    <product :premium="premium" @add-to-cart="updateCart"></product>
   </div>
 </template>
 
@@ -144,8 +187,8 @@
       }
     }, 
     methods: {
-        hatToggle() {
-          
+        addToCart () {
+          this.cart += 1
         },
       },
   }
